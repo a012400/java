85d7e41e3539c7a3be016dbd1f2dbaa8af44eec7
jkfx <377910451@qq.com>
2020-01-01 23:09:48 +0800

    2020-1-1

---
+++ eclipse-workspace/crazy-java/src/wildcard/ErrorTest.java
@@ -0,0 +1,22 @@
+package wildcard;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ErrorTest {
+	// 声明一个泛型方法，该泛型方法中带一个T泛型形参
+	static <T> void test(Collection<T> from, Collection<T> to) {
+		for (T ele : from) {
+			to.add(ele);
+		}
+	}
+
+	public static void main(String[] args) {
+		List<Object> ao = new ArrayList<>();
+		List<String> as = new ArrayList<>();
+		// 下面代码将产生编译错误
+		// test(as, ao);
+	}
+
+}
